8bc9ac2fdb30c639cac6035f4d56b18b94771e06
TRUEDEVIL666 <canhphong6868@gmail.com>
2025-01-18 21:12:33 +0700

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Login(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class Login extends StatefulWidget {
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final TextEditingController _emailController = TextEditingController(),
+      _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          body: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('lib/assets/images/backgrounds/login_background.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Center(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "CHEZ LUMIÈRE",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40,
+                        height: 4,
+                      ),
+                    ),
+                    TextField(
+                      controller: _emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                        hintText: 'yourmail@gmail.com',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        contentPadding: EdgeInsets.only(
+                          left: 10,
+                        ),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextField(
+                      controller: _passwordController,
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
+                        hintText: '*******',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        contentPadding: EdgeInsets.only(
+                          left: 10,
+                        ),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        gradient: const LinearGradient(
+                          colors: [
+                            Color.fromRGBO(181, 35, 58, 1.0),
+                            Color.fromRGBO(206, 42, 66, 1.0),
+                          ],
+                        ),
+                        borderRadius: BorderRadius.circular(80),
+                      ),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.black,
+                          backgroundColor: Colors.transparent,
+                          shadowColor: Colors.transparent,
+                          padding:
+                          EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                        ),
+                        onPressed: () {},
+                        child: const Text(
+                          "SIGN IN",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            color: Colors.black,
+                            thickness: 1,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Text('OR'),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Expanded(
+                          child: Divider(
+                            color: Colors.black,
+                            thickness: 1,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        foregroundColor: WidgetStateProperty.all(
+                          Colors.black,
+                        ),
+                        backgroundColor: WidgetStateProperty.all(
+                          const Color.fromRGBO(41, 182, 246, 1),
+                        ),
+                        textStyle: WidgetStateProperty.all(
+                          const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        "REGISTER",
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
+  }
+}
